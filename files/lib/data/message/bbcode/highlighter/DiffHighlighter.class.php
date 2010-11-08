2aa07c52de7562080814e9fa1aabeba44d2c19e1
Tim Düsterhus <timwolla@arcor.de>
2010-11-08 17:45:53 +0000

    Added diff and perl

---
+++ files/lib/data/message/bbcode/highlighter/DiffHighlighter.class.php
@@ -0,0 +1,49 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/data/message/bbcode/highlighter/Highlighter.class.php');
+
+/**
+ * Highlights diffs
+ *
+ * @author	Tim Düsterhus
+ * @copyright	Tim Düsterhus
+ * @package	---
+ * @license	
+ */
+class DiffHighlighter extends Highlighter {	
+	// keywords for an added line, the + is used in unified diff, the > in normal diff
+	protected $add = array("+", ">");
+	// keywords for an deleted line, the - is used in unified diff, the < in normal diff
+	protected $delete = array("-", "<");
+	// the "splitter" in changes for normal diff
+	protected $splitter = array("---");
+	// keywords for the line info, the @ is used in unfied diff, the numbers in normal diff
+	protected $info = array("@", '0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+	
+	/**
+	 * Overwrite method, cause we don't need that overhead
+	 * 
+	 * @see Highlighter::highlight();
+	 */
+	public function highlight($data) {
+		$lines = explode("\n", $data);
+		foreach ($lines as $key => $val) {
+			if (in_array(StringUtil::substring($val, 0,1), $this->info) || in_array($val, $this->splitter)) {
+				$lines[$key] = '<span style="'.$this->style['keywords3'].'">'.StringUtil::encodeHTML($val).'</span>';
+			}
+			else if (in_array(StringUtil::substring($val, 0,1), $this->add)) {
+				$lines[$key] = '<span style="'.$this->style['comments'].'">'.StringUtil::encodeHTML($val).'</span>';
+			}
+			else if (in_array(StringUtil::substring($val, 0,1), $this->delete)) {
+				$lines[$key] = '<span style="'.$this->style['keywords5'].'">'.StringUtil::encodeHTML($val).'</span>';
+			}
+			else {
+				$lines[$key] = StringUtil::encodeHTML($val);
+			}
+		}
+		
+		$data = implode("\n", $lines);
+		return $data;
+	}
+}
+?>
---
+++ files/lib/data/message/bbcode/highlighter/PerlHighlighter.class.php
@@ -0,0 +1,101 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/data/message/bbcode/highlighter/Highlighter.class.php');
+
+/**
+ * Highlights perl sourcecode
+ *
+ * @author	Tim Düsterhus
+ * @copyright	Tim Düsterhus
+ * @package	---
+ * @license	
+ */
+class PerlHighlighter extends Highlighter {
+	// highlighter syntax
+	protected $separators = array("(", ")", "{", "}", "[", "]", ";", ".", ",");
+	protected $singleLineComment = array("#");
+	protected $commentStart = array();
+	protected $commentEnd = array();
+	protected $operators = array(".=", "=", ">", "<", "!", "~", "?", ":", "==", "<=", ">=", "!=",
+		"&&", "||", "++", "--", "+", "-", "*", "/", "&", "|", "^", "%", "<<", ">>", ">>>", "+=", "-=", "*=",
+		"/=", "&=", "|=", "^=", "%=", "<<=", ">>=", ">>>=", "->", "::");
+	
+	protected $keywords1 = array(
+		"print",
+		"sprintf",
+		"length",
+		"substr",
+		"eval",
+		"die",
+		"opendir",
+		"closedir",
+		"open",
+		"close",
+		"chmod",
+		"unlink",
+		"flock",
+		"read",
+		"seek",
+		"stat",
+		"truncate",
+		"chomp",
+		"localtime",
+		"defined",
+		"undef",
+		"uc",
+		"lc",
+		"trim",
+		"split",
+		"sort",
+		"keys",
+		"push",
+		"pop",
+		"join",
+		"local",
+		"select",	
+		"index",
+		"sqrt",
+		"system",
+		"crypt",
+		"pack",
+		"unpack"
+	);
+	protected $keywords2 = array(
+		"case",
+		"do",
+		"while",
+		"for",
+		"if",
+		"foreach",
+		"my",
+		"else",
+		"elsif",
+		"eq",
+		"ne",
+		"or",
+		"xor",
+		"and",
+		"lt",
+		"gt",
+		"ge",
+		"le",
+		"return",
+		"last",
+		"goto",
+		"unless",
+		"given",
+		"when",
+		"default",
+		"until",
+		"break",
+		"exit"
+	);
+	
+	protected $keywords3 = array(
+		"use",
+		"import",
+		"require",
+		"sub"
+	);
+}
+?>
