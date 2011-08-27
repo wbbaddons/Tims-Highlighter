935599633d6330f3eb5d47a1b84931f70d573d0c
Tim Düsterhus <timwolla@arcor.de>
2011-08-27 21:15:28 +0200

    Adding hq9+

---
+++ files/lib/data/message/bbcode/highlighter/Hq9plusHighlighter.class.php
@@ -0,0 +1,38 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/data/message/bbcode/highlighter/Highlighter.class.php');
+
+/**
+ * Highlights HQ9+
+ *
+ * @author	Tim Düsterhus
+ * @copyright	2010 - 2011 Tim Düsterhus
+ * @package	timwolla.wcf.bbcode.highlighter
+ * @license 	Creative Commons BY-NC-SA <http://creativecommons.org/licenses/by-nc-sa/3.0/de/>
+ */
+class Hq9plusHighlighter extends Highlighter {
+	protected $separators = array('H', 'Q', '9', '+');
+	protected $quotes = array();
+	protected $singleLineComment = array();
+	protected $commentStart = array();
+	protected $commentEnd = array();
+	protected $escapeSequence = array();
+	
+	protected $keywords1 = array(
+		"H"
+	);
+	protected $keywords2 = array(
+		"Q"
+	);
+	protected $keywords3 = array(
+		"9"
+	);
+	protected $keywords4 = array(
+		"+"
+	);
+	
+	protected function highlightNumbers($string) {
+		// we don't want to highlight numbers
+		return $string;
+	}
+}
