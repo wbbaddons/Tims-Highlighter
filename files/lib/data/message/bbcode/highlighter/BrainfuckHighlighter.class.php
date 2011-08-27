196ff6d731fcd2f56f2a3477538b783f9831b7b3
Tim Düsterhus <timwolla@arcor.de>
2011-08-27 19:03:02 +0200

    Fixing Brainfuck highlighting of < and >

--- files/lib/data/message/bbcode/highlighter/BrainfuckHighlighter.class.php
+++ files/lib/data/message/bbcode/highlighter/BrainfuckHighlighter.class.php
@@ -13,11 +13,11 @@ require_once(WCF_DIR.'lib/data/message/bbcode/highlighter/Highlighter.class.php'
 class BrainfuckHighlighter extends Highlighter {
 	// highlighter syntax
 	protected $quotes = array();
-	protected $separators = array("[", "]", "+", "-", ".", ",");
+	protected $separators = array("[", "]", "+", "-", ".", ",", "<", ">");
 	protected $singleLineComment = array();
 	protected $commentStart = array();
 	protected $commentEnd = array();
-	protected $operators = array(">", "<");
+	protected $operators = array();
 
 	protected $keywords1 = array(
 		"+",
@@ -31,6 +31,10 @@ class BrainfuckHighlighter extends Highlighter {
 		"[",
 		"]"
 	);
+	protected $keywords4 = array(
+		">",
+		"<"
+	);
 	
 	protected function highlightNumbers($string) {
 		// we don't want to highlight numbers
@@ -43,6 +47,7 @@ class BrainfuckHighlighter extends Highlighter {
 	}
 
 	public function highlight($string) {
+		$string = StringUtil::encodeHTML($string);
 		// everything except the known chars is a comment
 		$string = preg_replace('~([^'.preg_quote(implode('', $this->separators), '~').']+)~', "<span style=\"".$this->style['comments']."\">\\1</span>", $string);
 		return parent::highlight($string);
