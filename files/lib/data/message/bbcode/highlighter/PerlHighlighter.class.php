c50790afd785be6b79f31e03cfce3acd6662a6a8
Tim Düsterhus <timwolla@arcor.de>
2011-08-31 19:34:14 +0200

    Changing packageName

--- files/lib/data/message/bbcode/highlighter/PerlHighlighter.class.php
+++ files/lib/data/message/bbcode/highlighter/PerlHighlighter.class.php
@@ -7,7 +7,7 @@ require_once(WCF_DIR.'lib/data/message/bbcode/highlighter/Highlighter.class.php'
  *
  * @author	Tim Düsterhus
  * @copyright	2010 - 2011 Tim Düsterhus
- * @package	timwolla.wcf.bbcode.highlighter
+ * @package	de.wbb3addons.timwolla.wcf.bbcode.highlighter
  * @license 	Creative Commons BY-NC-SA <http://creativecommons.org/licenses/by-nc-sa/3.0/de/>
  */
 class PerlHighlighter extends Highlighter {
